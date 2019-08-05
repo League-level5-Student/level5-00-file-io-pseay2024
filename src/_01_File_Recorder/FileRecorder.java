6cfdb2b317a903d30de6f0ede26952df39cad5f5
league <league@imac-18.attlocal.net>
2019-08-04 17:29:34 -0700

    encryptNext

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,21 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String s = JOptionPane.showInputDialog("Write a message");
+		try {
+			FileWriter fr = new FileWriter("src/_01_File_Recorder/test.txt");
+			fr.write(s);
+			fr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,6 +1,27 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.swing.JOptionPane;
+
 public class FileDecryptor {
 	// Create a program that opens the file created by FileEncryptor and display
 	// the decrypted message to the user in a JOptionPane.
+	public static void decrypt(String location)
+	{
+		try {
+			FileReader fr = new FileReader(location);
+			int c = fr.read();
+			String s = "";
+			while(c != -1)
+			{
+				s+=(char)(c - 1);
+			}
+			JOptionPane.showMessageDialog(null, s);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
